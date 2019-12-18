750213c8fe81da37fc7f6a400e5de865a7070af5
Kristjan Kalmet <kristjan.kalmet@ictpro.labra>
2019-12-18 15:01:06 +0200

    Luokka tehty

--- DemoTesti.ino
+++ DemoTesti.ino
@@ -1,40 +1,11 @@
-#define LED 5
-#define PAINIKE 6
-#define LEDON 255
-#define TASO1 180
-#define TASO2 100
-#define LEDOFF 0 
-#define FILTER 20
-int oldState;//painikkeen tilojen surantaa varten
-int valaistusTaso = LEDOFF;
+#include "fikkari.h"
+
+fikkari T1(5, 6);//luodaan ilmentymä meidän luokasta
 
 void setup(){
- pinMode(LED, OUTPUT);
- pinMode(PAINIKE, INPUT_PULLUP);
- oldState = digitalRead(PAINIKE);//alustetaan "muisijälki"
- Serial.begin(9600);
+
  }
-
-
-
-bool buttonIsPressed(){
-  bool retVal = false;
-  int x - digitalRead(PAINIKE);
- if(x==LOW && oldState == HIGH){//etsitään painikeen painallus
-  retVal - true;
- }
- oldState = x;
- delay(FILTER);
- return retVal;
-}
 
 void loop(){
-  if(buttonIsPressed()){
-    switch(valaistusTaso);
-     case LEDOFF:valaistusTaso=LEDON;break;
-     case LEDON:valaistusTaso=TASO1;break;
-     case TASO1:valaistusTaso=TASO2;break;
-     case TASO2:valaistusTaso=LEDOFF;break;
-  }
-  analogWrite(LED, valaistusTaso);
+  T1.handle();
 }
